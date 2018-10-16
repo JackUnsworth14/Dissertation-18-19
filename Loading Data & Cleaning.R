e75aca7bebdca1bf0085f5f123e3f42c22d8fb70
Jack Unsworth <jackunsworth@hotmail.co.uk>
2018-10-16 20:48:34 +0100

    Add files via upload

--- Loading Data & Cleaning.R
+++ Loading Data & Cleaning.R
@@ -1,5 +1,11 @@
-PL1516 <- read.csv('PL1516.csv')
+# We use data from http://www.football-data.co.uk/englandm.php, this is updated weekly
+# The descriptions of variables are in the file http://www.football-data.co.uk/notes.txt
+
+PL1516 <- read.csv('PL1516.csv')  # Loading Data
 PL1617 <- read.csv('PL1617.csv')
 PL1718 <- read.csv('PL1718.csv')
 PL1819 <- read.csv('PL1819.csv')
+
+
+
 
